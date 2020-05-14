67dd1094220fff8aa8c5c5784700882049520d08
Hossein Mohebbi <mhmohebbi@gmail.com>
2020-05-13 22:18:44 -0400

    Create main.cc
    main.cc

---
+++ main.cc
@@ -0,0 +1,7 @@
+  #include <iostream>
+  using namespace std;
+
+  int main()                       
+  {                            
+    cout << "Due to the university's academic integrity policies, the source code can be seen upon request :)" << endl; 
+  }
